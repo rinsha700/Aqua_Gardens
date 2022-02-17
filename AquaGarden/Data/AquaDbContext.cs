4f818eb9f03089b5ca28b4dfd45efcd7316ff626
SH-PKD-007 <SH-PKD-007@DESKTOP-OR85DOS>
2022-02-16 21:15:38 -0800

    Add project files.

---
+++ AquaGarden/Data/AquaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AquaGarden.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AquaGarden.Data
+{
+    public class AquaDbContext:DbContext
+    {
+        public AquaDbContext(DbContextOptions<AquaDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Aqua> Aquas { get; set; }
+    }
+}
---
+++ AquaGarden/Models/Aqua.cs
@@ -0,0 +1,11 @@
+﻿namespace AquaGarden.Models
+{
+    public class Aqua
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
